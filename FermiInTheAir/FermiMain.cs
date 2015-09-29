4cf00ad64f62fd846169061a2df55e0a1e96ff1c
randall <rolandchain@gmail.com>
2015-09-29 15:42:00 +0300

    Ross' music moved to utility, added as thread in Main

--- FermiInTheAir/FermiMain.cs
+++ FermiInTheAir/FermiMain.cs
@@ -1,11 +1,15 @@
-﻿namespace FermiInTheAir
+﻿using System.Threading;
+
+namespace FermiInTheAir
 {
    public class FermiMain
     {
         public static void Main()
         {
+            Music playMusic = new Music();
+            Thread music = new Thread(playMusic.Play);
+            music.Start();
             Engine eng = new Engine();
-
             eng.Run();
         }
     }
---
+++ FermiInTheAir/Utility/Music.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermiInTheAir
+{
+    public class Music
+    {
+        private static string Source = "http://www.thesoundarchive.com/starwars/imperial_march.wav";
+
+        public Music()
+        {
+            this.Play();
+        }
+
+        public void Play()
+        {
+            SoundPlayer player = new SoundPlayer(Source);
+
+            using (player)
+            {
+                player.PlayLooping();
+            }
+        }
+    }
+}
