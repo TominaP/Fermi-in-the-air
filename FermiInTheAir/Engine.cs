0ea8041bc667b5c74009e52a0f1d681e2db1f30a
Todor Vasilev <bggadbg@abv.bg>
2015-09-30 22:25:08 +0300

    Added Pause Screen

--- FermiInTheAir/Engine.cs
+++ FermiInTheAir/Engine.cs
@@ -1,4 +1,4 @@
-﻿using FermiInTheAir.GameObjects;
+using FermiInTheAir.GameObjects;
 using FermiInTheAir.Utility;
 using System;
 using System.Collections.Generic;
@@ -6,6 +6,86 @@ using System.Threading;
 
 namespace FermiInTheAir
 {
+    class PauseScreenPage
+    {
+         public static void PauseMain()
+        {
+            Header();
+            Footer();
+
+
+            Console.ReadKey(true);
+            if (true)
+            {
+                //the game is starting again
+            }
+        }
+
+        static void Header()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(new string('*', 80));
+            }
+
+            WritingLines(5);
+
+            string[] team =
+            {
+            "             !$$$$$$$!  |#######|   %%%@@@@     &&&++    ++&&&   XXX            ",
+            "             !$$!       |##|        %%%   @@    &&& ++  ++ &&&   XXX            ",
+            "             !$$!       |##|        %%%  @@@    &&&   ++   &&&   XXX            ",
+            "             !$$$$$$$!  |#######|   %%%@@@      &&&        &&&   XXX            ",
+            "             !$$!       |##|        %%% @@@     &&&        &&&   XXX            ",
+            "             !$$!       |#######|   %%%  @@@    &&&        &&&   XXX            ",
+        };
+            for (int i = 0; i < team.Length; i++)
+            {
+                Console.WriteLine(team[i]);
+            }
+            Console.WriteLine();
+            string[] logo =
+            {
+            @"       ()  1\\   1     <<<<>>>> ||   ||  010101          //\\     /\  ??&&      ",
+            @"       ()  1 \\  1        ^^    ||___||  01,,,          //  \\    /\  ?? @@     ",
+            @"       ()  1  \\ 1        ^^    ||   ||  01'''         //====\\   /\  ??%%      ",
+            @"       ()  1   \\1        ^^    ||   ||  010101       //      \\  /\  ?? ##     "
+        };
+            for (int i = 0; i < logo.Length; i++)
+            {
+                Console.WriteLine(logo[i]);
+            }
+            WritingLines(3);
+        }
+        static void Footer()
+        {
+            WritingLines(5);
+            Console.ForegroundColor = ConsoleColor.Red;
+            string paused = "The game is paused";
+            Console.WriteLine("{0}{1}{0}", new string(' ', (80 - paused.Length - 1) / 2), paused);
+            string printing = "Press any key to continue:";
+            Console.WriteLine("{0}{1}{0}", new string(' ', (80 - printing.Length - 1) / 2), printing);
+
+            WritingLines(8);
+
+            string producers = "One game produced by team Fermi, SoftUni";
+            Console.WriteLine("{0}{1}{0}", new string(' ', (80 - producers.Length - 1) / 2), producers);
+            Console.WriteLine("{0}{1}{0}", new string(' ', 74 / 2), "2015");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(new string('*', 80));
+            }
+        }
+        static void WritingLines(int line)
+        {
+            for (int i = 0; i < line; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+
     public class Engine
     {
         private Queue<GameObject> gameObjectsList = new Queue<GameObject>();
@@ -65,7 +145,7 @@ namespace FermiInTheAir
                         newGameObjectsList.Enqueue(current);
                     }
                 }
-               
+
                 gameObjectsList = newGameObjectsList;
                 newGameObjectsList = new Queue<GameObject>();
 
@@ -125,7 +205,8 @@ namespace FermiInTheAir
 
                     if (keyPressed.Key == ConsoleKey.P)
                     {
-                        //TODO : Print pause screen
+                        //added PauseScreen
+                        PauseScreenPage pause = new PauseScreenPage();
                         settings.Pause = !settings.Pause;
                         Console.ReadKey();
                     }
@@ -184,7 +265,7 @@ namespace FermiInTheAir
 
                 gameObjectsList = newGameObjectsList;
                 newGameObjectsList = new Queue<GameObject>();
-               
+
 
                 Thread.Sleep(sleepTime);
             }
@@ -193,4 +274,3 @@ namespace FermiInTheAir
         }
     }
 }
-
